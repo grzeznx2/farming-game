75baa8cf597d0fe9880f285d10a807c83f1256f5
grzeznx2 <grzeznx2@gmail.com>
2022-11-03 18:13:12 +0100

    Add Cinemachine Confiner

---
+++ Assets/Scripts/Scene/SwitchConfineBoundingShape.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using Cinemachine;
+
+public class SwitchConfineBoundingShape : MonoBehaviour
+{
+
+    void Start()
+    {
+        SwitchBoundingShape();    
+    }
+
+    private void SwitchBoundingShape()
+    {
+
+        PolygonCollider2D polygonCollider2D = GameObject.FindGameObjectWithTag(Tags.BoundsConfiner).GetComponent<PolygonCollider2D>();
+
+        CinemachineConfiner cinemachineConfiner = GetComponent<CinemachineConfiner>();
+
+        cinemachineConfiner.m_BoundingShape2D = polygonCollider2D;
+
+        cinemachineConfiner.InvalidatePathCache();
+    }
+}
