5d31010eaf3e8361f920bf940c42bdafd94f1764
grzeznx2 <grzeznx2@gmail.com>
2022-11-05 11:05:32 +0100

    Create Scenery Fader

---
+++ Assets/Scripts/Item/TriggerObscuringItemFader.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class TriggerObscuringItemFader : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Get the GO we have collidded with, and then all the OIF components on it and its children
+        ObscuringItemFader[] obscuringItemFaders = collision.gameObject.GetComponentsInChildren<ObscuringItemFader>();
+
+        if(obscuringItemFaders.Length > 0)
+        {
+            for(int i = 0; i < obscuringItemFaders.Length; i++)
+            {
+                obscuringItemFaders[i].FadeOut();
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        ObscuringItemFader[] obscuringItemFaders = collision.gameObject.GetComponentsInChildren<ObscuringItemFader>();
+
+        if(obscuringItemFaders.Length > 0)
+        {
+            for(int i = 0; i < obscuringItemFaders.Length; i++)
+            {
+                obscuringItemFaders[i].FadeIn();
+            }
+        }
+    }
+}
